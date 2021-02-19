194368bc429525c51123f2b84043f23f7db5ba0e
rizkyfarhanrr <30488459+rizkyfarhanrr@users.noreply.github.com>
2021-02-20 06:26:23 +0700

    Update install_py.sh

--- install_py.sh
+++ install_py.sh
@@ -5,80 +5,53 @@ echo """
 #==========================#
 """
 
-if [[ "$*" =~ storage || "$*" =~ all ]]; then
-	termux-setup-storage
-fi
+
 
 vers=`uname -m`
 DIRECTORY_PATH="rzhan_python_installer"
 mkdir -p $DIRECTORY_PATH
 
 startup () {
+    termux-setup-storage
     pkg update && pkg upgrade
-    pkg install curl
+    apt install curl
     check_python
 }
 check_python () {
     pyv="$(python -V 2>&1)"
     echo "Checking python version Please Wait....."
-    echo " "
     sleep 2
     if ! command -v python &> /dev/null
     then
-        
-        echo " "
-        echo "python is not available"
-        echo "want to install python 3.8.6 ?"
-        echo " "
-        while true
-        do
-            read -r -p "Yes or No? [Y/n] " input
-
-            case $input in
-                [yY][eE][sS]|[yY])
-            download_install
-            break
-            ;;
-                [nN][oO]|[nN])
-            echo " "
-            echo "Ok Exiting program ... "
-            sleep 2
-            exit
-                   ;;
-                *)
-            echo " "
-            echo "you can only choose [Y]es or [N]o"
-            ;;
-            esac
+        echo "python is not available, want to install python 3.8.6 ?"
+        select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) download_install; break;;
+            No ) exit;;
+        esac
         done
     else
-        echo " "
-        echo "your python is available as $pyv --version"
-        echo "want to reinstall with 3.8.6 --version?\n"
-        echo " "
-        while true
-        do
-            read -r -p "Yes or No [Y/n] " input
-
-            case $input in
-                [yY][eE][sS]|[yY])
-            pkg uninstall python -y
-            download_install
-            break
-            ;;
-                [nN][oO]|[nN])
-            echo " "
-            echo "Ok Exiting program ... "
-            sleep 2
-            exit
-                    ;;
-                *)
-            echo " "
-            echo "you can only choose [Y]es or [N]o"
-            ;;
-         esac
+        echo "your python is available as $pyv --version, want to reinstall with 3.8.6 --version?"
+        select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) download_reinstall; break;;
+            No ) exit;;
+        esac
         done
     fi
+}
+download_reinstall (){
+
+    pkg uninstall python -y
+
+    curl -o $DIRECTORY_PATH/python_static.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python-static_3.8.6_aarch64.deb
+    curl -o $DIRECTORY_PATH/python_tkinter.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python-tkinter_3.8.6_aarch64.deb
+    curl -o $DIRECTORY_PATH/python_3.8.6.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python_3.8.6_aarch64.deb
+
+    for d in $(ls $DIRECTORY_PATH); do apt install $DIRECTORY_PATH/./$d ; echo "$d Succesfully Installed"; done
+    
+    rm -r $DIRECTORY_PATH
+    echo "installation file has been deleted"
 }
 
 download_install (){
