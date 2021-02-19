90be7eae28bcba219d38dadb1984c9b24f76f1a0
rizkyfarhanrr <30488459+rizkyfarhanrr@users.noreply.github.com>
2021-02-20 06:55:05 +0700

    Update install_py.sh

--- install_py.sh
+++ install_py.sh
@@ -7,56 +7,36 @@ echo """
 vers=`uname -m`
 DIRECTORY_PATH="python_installer"
 mkdir -p $DIRECTORY_PATH
-pyv="$(python -V 2>&1)"
-termux-setup-storage
+
 pkg update && pkg upgrade
 pkg install curl
+
 echo "Checking python version ....."
-sleep 2 s
 if ! command -v python &> /dev/null
 then
-    echo "python is not available, want to install python 3.8.6 ?"
-    select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) download_install; break;;
-        No ) exit;;
-    esac
-    done
+    echo ""
+    echo "python is not available"
+    echo "installing python 3.8.6 ... "
+    echo ""
+    download_install()
+    
 else
-    echo "your python is available as $pyv --version, want to reinstall with 3.8.6 --version?"
-    select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) download_reinstall; break;;
-        No ) exit;;
-    esac
-    done
+    pyv="$(python -V 2>&1)"
+    echo ""
+    echo "your python is available as $pyv --version"
+    echo "reinstall with 3.8.6 --version "
+    echo ""
+    pkg uninstall python -y
+    download_install()
 fi
 
-
-download_reinstall (){
-
-    pkg uninstall python -y
-
+download_install() {
     curl -o $DIRECTORY_PATH/python_static.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python-static_3.8.6_aarch64.deb
     curl -o $DIRECTORY_PATH/python_tkinter.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python-tkinter_3.8.6_aarch64.deb
     curl -o $DIRECTORY_PATH/python_3.8.6.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python_3.8.6_aarch64.deb
-
     for d in $(ls $DIRECTORY_PATH); do apt install $DIRECTORY_PATH/./$d ; echo "$d Succesfully Installed"; done
-    
+
     rm -r $DIRECTORY_PATH
+
     echo "installation file has been deleted"
 }
-
-download_install (){
-
-    curl -o $DIRECTORY_PATH/python_static.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python-static_3.8.6_aarch64.deb
-    curl -o $DIRECTORY_PATH/python_tkinter.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python-tkinter_3.8.6_aarch64.deb
-    curl -o $DIRECTORY_PATH/python_3.8.6.deb https://raw.githubusercontent.com/Termux-pod/termux-pod/main/$vers/python/python-3.8.6/python_3.8.6_aarch64.deb
-
-    for d in $(ls $DIRECTORY_PATH); do apt install $DIRECTORY_PATH/./$d ; echo "$d Succesfully Installed"; done
-    
-    rm -r $DIRECTORY_PATH
-    echo "installation file has been deleted"
-}
-
-startup
